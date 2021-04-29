f22f0c6f70dbe2863b4a41ce13d57032d3cdf4d5
JosBenn1 <benni606@hotmail.com>
2021-04-29 16:15:34 -0500

    prueba

---
+++ appVoto/urls.py
@@ -0,0 +1,30 @@
+"""appVoto URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('scanner/', views.scanner, name="scanner"),
+    path('scannerResult/', views.scannerResult, name="scannerResult"),
+    path('procesosElectorales/', views.procesosElectorales, name="procesosElectorales"),
+    path('boleta/', views.boleta, name="boleta"),
+    path('generadorVoto/', views.generadorVoto, name="generadorVoto"),
+    path('votoQr/', views.votoQr, name="votoQr"),
+    path('boletaInside/', views.boletaInside, name="boletaInside"),
+    path('admin/', admin.site.urls),
+]
---
+++ core/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "core/index.html")
+
+def scanner(request):
+    return render(request, "core/scanner.html")
+
+def scannerResult(request):
+    return render(request, "core/scanner-result.html")
+
+def procesosElectorales(request):
+    return render(request, "core/procesos-electorales.html")
+
+def boleta(request):
+    return render(request, "core/boleta.html")
+
+def generadorVoto(request):
+    return render(request, "core/generador-voto.html")
+
+def votoQr(request):
+    return render(request, "core/voto-qr-resultado.html")
+
+def boletaInside(request):
+    return render(request, "core/boleta-inside.html")
