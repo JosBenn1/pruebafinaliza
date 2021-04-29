c6e811510f8338c8ca2407b9153f625743f7f41b
JosBenn1 <benni606@hotmail.com>
2021-04-29 17:47:51 -0500

    a

--- appVoto/settings.py
+++ appVoto/settings.py
@@ -123,3 +123,8 @@ STATIC_DIRS = (os.path.join(BASE_DIR, 'core/static/core'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static'),
+]
--- appVoto/urls.py
+++ appVoto/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name="index"),
